3bd8c39f9fb9eef2a904237bce8a1df8ee016a10
Benjamin Valdes Madriaza <benjoks@MacBook-Air-de-Benjamin.local>
2022-08-30 16:54:25 -0400

    Insert many data with Axios as provider

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
+
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
@@ -11,15 +12,14 @@ export class SeedService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel:Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ){}
-
-  private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
 
     await this.pokemonModel.deleteMany({}); // DELETE * FROM POKEMONS
 
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
     const pokemonToInsert: {name:string, no: number}[] =[];
 
